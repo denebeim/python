f5815765825bea1b7ed3d20adf79511199a6e004
Joy Denebeim <denebeim@gmail.com>
2024-07-09 23:50:35 -0700

    Model for list Items and associated migration

--- functional_test.py
+++ functional_test.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
