318ca8d0dd01719fa3bf49900146ae87c7285069
Joy Denebeim <denebeim@gmail.com>
2024-07-10 23:17:18 -0700

    Adjust model to plase all items in different lists

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import redirect, render
 
-from lists.models import Item
-import urllib
+from lists.models import Item, List
 
 
 # Create your views here.
@@ -15,5 +14,6 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST["item_text"])
+    nulist=List.objects.create()
+    Item.objects.create(text=request.POST["item_text"],list=nulist)
     return redirect("/lists/the-only-list-in-the-world/")
