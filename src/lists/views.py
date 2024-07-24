17674c4786a8988028a4b3fda97199e9e584310f
Joy Denebeim <denebeim@gmail.com>
2024-07-23 18:49:13 -0700

    form functioning properly.  Looks production ready.

--- src/lists/views.py
+++ src/lists/views.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 
-from .forms import ItemForm, EMPTY_ITEM_ERROR
+from .forms import EMPTY_ITEM_ERROR, ExistingListItemForm, ItemForm
 from .models import Item, List
 
 
@@ -22,12 +22,13 @@ def new_list(request):
 
 def view_list(request, list_id):
     our_list = List.objects.get(id=list_id)
+    form=ExistingListItemForm(for_list=our_list)
 
     if request.method == "POST":
-        form = ItemForm(data=request.POST)
+        form = ExistingListItemForm(for_list=our_list,data=request.POST)
         if form.is_valid():
-            form.save(for_list=our_list)
+            form.save()
             return redirect(our_list)
     else:
-        form = ItemForm()
+        form = ExistingListItemForm(for_list=our_list)
     return render(request, "list.html", {"list": our_list, "form": form})
