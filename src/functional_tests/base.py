3edb514e0c728284a9f81ce7903febc89ff48c99
Joy Denebeim <denebeim@gmail.com>
2024-07-26 18:48:21 -0700

    Use decorators for waits

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -9,6 +9,21 @@ from selenium.webdriver.common.by import By
 MAX_WAIT = 5
 
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.1)
+
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,16 +34,9 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    @wait
     def wait_for(self, fn):
-        start_time = time.time()
-
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException):
-                if time.time() - start_time > MAX_WAIT:
-                    raise
-                time.sleep(0.1)
+        return fn()
 
     def row_in_list_table(self, row_text):
         table = self.browser.find_element(By.ID, "id_list_table")
--- src/functional_tests/test_my_lists.py
+++ src/functional_tests/test_my_lists.py
@@ -3,31 +3,36 @@ from django.contrib.sessions.backends.db import SessionStore
 
 from superlists import settings
 
-from .base import FunctionalTest
+from .base import FunctionalTest, wait
 
 User = get_user_model()
 
 
 class MyListsTest(FunctionalTest):
+    @wait
     def create_pre_authenticated_session(self, email):
         user = User.objects.create(email=email)
         session = SessionStore()
-        session[SESSION_KEY]=user.pk
-        session[BACKEND_SESSION_KEY]=settings.AUTHENTICATION_BACKENDS[0]
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
         session.save()
         ## to set a cookie we need to first visit the domain,
         ## 404 pages load the quickest!
-        self.browser.get(self.live_server_url+"/404_no_such_url/")
-        self.browser.add_cookie(dict(
-            name=settings.SESSION_COOKIE_NAME,
-            value=session.session_key,
-            path='/',
-        ))
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
+        self.browser.add_cookie(
+            dict(
+                name=settings.SESSION_COOKIE_NAME,
+                value=session.session_key,
+                path="/",
+            )
+        )
+
+    @wait
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
-        email="edith@example.com"
+        email = "edith@example.com"
         self.browser.get(self.live_server_url)
         self.wait_to_be_logged_out(email)
-        
+
         # Edith is a logged-in user
         self.create_pre_authenticated_session(email)
         self.browser.get(self.live_server_url)
